78a6e0d1b7376f7b6a9961c1902347c822e92e51
Alexandre Junior <165928509+alexandrejr-tech@users.noreply.github.com>
2024-04-16 22:59:08 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function cliqueiNoBotao () {
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function cliqueiNaMascara () {
+    mascara.style.visibility = "hidden"
+    form.style.left = "-330px"
+    form.style.transform = "translateX(0)"
+}
+    
+    
+    
